4ef90858b1e23fc1e61de5810e41740222d8aeac
byTrusty <112107770+byTrusty@users.noreply.github.com>
2023-03-06 21:16:07 -0500

    update 2

--- main.py
+++ main.py
@@ -11,20 +11,24 @@ desiredTemp = input('What is the desired temp? ')
 print(heating_cooling(actualTemp,desiredTemp))
 
 
+def celsius_conversion(tempCelsius, targetUnit):
+    if targetUnit == "C":
+        convertedTemp = tempCelsius
+    elif targetUnit == "F":
+        convertedTemp = (tempCelsius * 9/5) + 32
+    elif targetUnit == "K":
+        convertedTemp = tempCelsius + 273.15
+    else:
+        print("Invalid target unit. Please choose 'C', 'F', or 'K'.")
+        convertedTemp = None
+    if convertedTemp is not None:
+        print(f"The temperature is {convertedTemp} {targetUnit}.")
+        print(f"You entered {tempCelsius} Celsius.")
+
+
+
 tempStr = input('Enter the temperature in Celsius: ')
 tempCelsius = float(tempStr)
 targetUnit = input('What unit would you like this converted to? (C/F/K): ')
+print(celsius_conversion(tempCelsius,targetUnit))
 
-if targetUnit == "C":
-    convertedTemp = tempCelsius
-elif targetUnit == "F":
-    convertedTemp = (tempCelsius * 9/5) + 32
-elif targetUnit == "K":
-    convertedTemp = tempCelsius + 273.15
-else:
-    print("Invalid target unit. Please choose 'C', 'F', or 'K'.")
-    convertedTemp = None
-
-if convertedTemp is not None:
-    print(f"The temperature is {convertedTemp} {targetUnit}.")
-    print(f"You entered {tempCelsius} Celsius.")
